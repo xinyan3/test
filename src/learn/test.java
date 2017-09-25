d454610afd9ade9d4b305ab2bc60afab9fa3a47c
xinyan3 <sky831202@gmail.com>
2017-09-25 09:41:26 +0800

    test

--- src/learn/test.java
+++ src/learn/test.java
@@ -5,6 +5,10 @@ public class test {
 
 	public static void main(String[] args){
 	    System.out.println(cc.q);
+	    Thread mythread=new Thread();
+	    System.out.println(mythread.getId()+mythread.getName()+mythread.getContextClassLoader());
+	    mythread.run();
+
 	
 	    System.out.println(cc.b);
 }
